8bf69e55b861cbb6ca82593383ec9c393fc6aa61
dinghenc <308024784@qq.com>
2024-07-28 17:26:01 +0800

    feat: LRU cache

--- cache/lru.go
+++ cache/lru.go
@@ -1,32 +1,58 @@
 package cache
 
-type LRU struct {
-	head *ListNode
-	tail *ListNode
-	m    map[int]*ListNode
+type LRUCache struct {
+	capacity   int
+	head, tail *ListNode
+	m          map[int]*ListNode
+}
+
+func NewLRUCache(capacity int) LRUCache {
+	head, tail := NewNode()
+	return LRUCache{
+		capacity: capacity,
+		head:     head,
+		tail:     tail,
+		m:        make(map[int]*ListNode),
+	}
+}
+
+func (c *LRUCache) Get(key int) int {
+	node, ok := c.m[key]
+	if !ok {
+		return -1
+	}
+	node = node.Remove()
+	c.head.Insert(node)
+	return node.Val
+}
+
+func (c *LRUCache) Put(key int, value int) {
+	node, ok := c.m[key]
+	if ok {
+		node = node.Remove()
+		node.Val = value
+	} else {
+		if c.capacity == len(c.m) {
+			del := c.tail.prev.Remove()
+			delete(c.m, del.Key)
+		}
+		node = &ListNode{Key: key, Val: value}
+		c.m[key] = node
+	}
+	c.head.Insert(node)
 }
 
 type ListNode struct {
-	prev *ListNode
-	next *ListNode
-	Val  int
+	Key, Val   int
+	prev, next *ListNode
 }
 
-func NewListNode() (*ListNode, *ListNode) {
+func NewNode() (*ListNode, *ListNode) {
 	head := new(ListNode)
 	tail := new(ListNode)
 	head.next = tail
 	tail.prev = head
 	return head, tail
-}
-
-func NewLRU() *LRU {
-	head, tail := NewListNode()
-	return &LRU{
-		head: head,
-		tail: tail,
-		m:    make(map[int]*ListNode),
-	}
 }
 
 func (l *ListNode) Insert(node *ListNode) {
@@ -39,28 +65,14 @@ func (l *ListNode) Insert(node *ListNode) {
 func (l *ListNode) Remove() *ListNode {
 	l.prev.next = l.next
 	l.next.prev = l.prev
-	l.prev = nil
 	l.next = nil
+	l.prev = nil
 	return l
 }
 
-func (l *LRU) Get(key int) int {
-	node, ok := l.m[key]
-	if !ok {
-		return -1
-	}
-	node = node.Remove()
-	l.head.Insert(node)
-	return node.Val
-}
-
-func (l *LRU) Set(key int, val int) {
-	node, ok := l.m[key]
-	if !ok {
-		node = &ListNode{Val: val}
-		l.m[key] = node
-	} else {
-		node.Remove()
-	}
-	l.head.Insert(node)
-}
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * obj := Constructor(capacity);
+ * param_1 := obj.Get(key);
+ * obj.Put(key,value);
+ */
